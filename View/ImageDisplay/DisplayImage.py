5689ead882924e1fa4106914199ad5e051bd5c88
Andrew Storus <andrewstorus@gmail.com>
2016-12-16 22:46:08 -0500

    midway through some display mods

--- View/ImageDisplay/DisplayImage.py
+++ View/ImageDisplay/DisplayImage.py
@@ -1,10 +1,10 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.scatterlayout import ScatterLayout
 from kivy.uix.image import Image
+from kivy.uix.button import Button
+import time
 
 kivy.require('1.9.1')
 
@@ -12,6 +12,7 @@ class DisplayImage(App):
     layout = None
     num_keys = 0
     keys_displayed = []
+    keys_displayed_copy = []
     side_buffer = 0
     key_height = 0
 
@@ -23,25 +24,29 @@ class DisplayImage(App):
 
     def set_layout(self):
         self.layout = FloatLayout()
+        backgroundColor = Button(Color=(0.7 , 0, 0))
+        backgroundColor.disabled = True
+        self.layout.add_widget(backgroundColor)
 
     def add_keys_to_board(self, num_keys):
         self.set_layout()
         self.set_num_keys(num_keys)
-        key_images = []
+        key_widgets = []
         horizontal_offset = 0
 
         for i in range(num_keys):
             if i % 12 == 0 or i % 12 == 5:
-                key_images.append(Image(source="KeyImages/LeftSideSkinny.png"))
+                key_widgets.append(Image(source="KeyImages/LeftSideSkinny.png"))
             if i % 12 == 1 or i % 12 == 3 or i % 12 == 6 or i % 12 == 8 or i % 12 == 10:
-                key_images.append(Image(source="KeyImages/BlackKey.png"))
+                key_widgets.append(Image(source="KeyImages/BlackKey.png"))
             if i % 12 == 2 or i % 12 == 7 or i % 12 == 9:
-                key_images.append(Image(source="KeyImages/MiddleSkinny.png"))
+                key_widgets.append(Image(source="KeyImages/MiddleSkinny.png"))
             if i % 12 == 4 or i % 12 == 11:
-                key_images.append(Image(source="KeyImages/RightSideSkinny.png"))
-            self.insert_to_layout_leftmost(key_images[i], horizontal_offset=horizontal_offset)
-            horizontal_offset += 50
-
+                key_widgets.append(Image(source="KeyImages/RightSideSkinny.png"))
+            self.keys_displayed[i] = True
+            self.insert_to_layout_leftmost(key_widgets[i], horizontal_offset=horizontal_offset)
+            horizontal_offset += key_widgets[i].width
+        self.key_widgets = key_widgets
 
     def insert_to_layout_leftmost(self, widget, horizontal_offset=0):
         widget.height = self.key_height
@@ -56,21 +61,23 @@ class DisplayImage(App):
     def fullscreen(self):
         Window.fullscreen = True
 
+    def toggleKeyDisplay(self, buttonNum):
+        if buttonNum < self.num_keys:
+            if self.keys_displayed[buttonNum] is True:
+                self.keys_displayed[buttonNum] = False
+                self.key_widgets[buttonNum].x -= 10000
+            elif self.keys_displayed[buttonNum] is False:
+                self.keys_displayed[buttonNum] = True
+                self.key_widgets[buttonNum].x += 10000
+
 display = DisplayImage()
 display.fullscreen()
-display.add_keys_to_board(13)
-'''
-key1 = Image(source="KeyImages/LeftSideSkinny.png")
-#key1.size_hint_x = 0.5
-#key1.size_hint_y = 0.5
-
-display.insert_to_layout_leftmost(key1)
-
-key2 = Image(source="KeyImages/RightSideSkinny.png")
-key2.pos = (100,0)
-display.insert_to_layout_leftmost(key2)
-'''
-
-
+display.add_keys_to_board(8)
+print display.key_widgets[2].right
+display.toggleKeyDisplay(2)
+print display.key_widgets[2].right
 
 display.run()
+display.toggleKeyDisplay(2)
+
+print display.key_widgets[2].right
